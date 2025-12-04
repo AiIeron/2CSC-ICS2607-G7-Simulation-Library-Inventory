8b27b6661f8312767f221b74bc1adc80469c0381
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-04 20:41:03 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,16 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Logging Out...</title>
+    <meta http-equiv="refresh" content="1; URL=login.php">
+</head>
+<body>
+    <p>You have been logged out. Redirecting...</p>
+</body>
+</html>
