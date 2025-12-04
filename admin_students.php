02e2eae04abac36d1b70145d5e845238e4825437
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-04 21:37:27 +0800

    Create admin_students.php

---
+++ admin_students.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manage Students</title>
+</head>
+<body>
+
+<h2>Students</h2>
+
+<a href="add_student.php">Add Student</a>
+
+<table border="1">
+    <tr>
+        <th>ID Number</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Actions</th>
+    </tr>
+</table>
+
+</body>
+</html>
