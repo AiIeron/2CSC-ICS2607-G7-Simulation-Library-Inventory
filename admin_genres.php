2b4e4582eee8a2045bce0b4ddadbf2aa189cea1f
AiIeron <robaileron.delarosa.cics@ust.edu.ph>
2025-12-05 03:14:13 +0800

    Update links for editing and deleting genres

--- admin_genres.php
+++ admin_genres.php
@@ -30,8 +30,8 @@ while($row = $res->fetch_assoc()) {
         <td>{$row['GENRE_NAME']}</td>
         <td>{$row['GENRE_DESC']}</td>
         <td>
-            <a href='TOTAL.php?edit={$row['STU_ID_NUM']}'>Edit</a> |
-            <a href='TOTAL.php?delete={$row['STU_ID_NUM']}' onclick='return confirm(\"Delete order?\")'>Delete</a>
+            <a href='edit.php'>Edit</a> |
+            <a href='delete.php' onclick='return confirm(\"Delete order?\")'>Delete</a>
         </td>
     </tr>";
 }
