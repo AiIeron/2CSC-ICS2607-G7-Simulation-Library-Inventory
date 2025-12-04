1d901678673d205221ab982c4881141d9bf8fb61
AiIeron <robaileron.delarosa.cics@ust.edu.ph>
2025-12-05 02:58:38 +0800

    Fetch and display books from the database

--- admin_books.php
+++ admin_books.php
@@ -1,3 +1,7 @@
+<?php 
+require 'db.php'
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,6 +22,23 @@
         <th>Publisher</th>
         <th>Actions</th>
     </tr>
+        <?php
+$res = $conn->query("SELECT * FROM BOOK");
+
+while($row = $res->fetch_assoc()) {
+    echo "<tr>
+        <td>{$row['BOOK_ID']}</td>
+        <td>{$row['BOOK_TITLE']}</td>
+        <td>{$row['BOOK_AGE_RATING']}</td>
+        <td>{$row['BOOK_STATUS']}</td>
+        <td>{$row['PUB_ID']}</td>
+        <td>
+            <a href='TOTAL.php?edit={$row['STU_ID_NUM']}'>Edit</a> |
+            <a href='TOTAL.php?delete={$row['STU_ID_NUM']}' onclick='return confirm(\"Delete order?\")'>Delete</a>
+        </td>
+    </tr>";
+}
+?>
 </table>
 
 </body>
