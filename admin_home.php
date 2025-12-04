ced81a7e182e161c6d03bdcec914b043cffa1c42
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-04 21:35:02 +0800

    Update admin_home.php

--- admin_home.php
+++ admin_home.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Home</title>
+</head>
+<body>
 
+<h2>Admin Home</h2>
+
+<ul>
+    <li><a href="admin_books.php">Manage Books</a></li>
+    <li><a href="admin_students.php">Manage Students</a></li>
+    <li><a href="admin_authors.php">Manage Authors</a></li>
+    <li><a href="admin_publishers.php">Manage Publishers</a></li>
+    <li><a href="admin_genres.php">Manage Genres</a></li>
+    <li><a href="admin_rentals.php">Manage Rentals</a></li>
+</ul>
+
+</body>
+</html>
