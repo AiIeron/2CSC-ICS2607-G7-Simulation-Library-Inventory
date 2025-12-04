feeb6e4c38e62860b61c7bd22d83a70862cdf9e0
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-04 21:35:50 +0800

    Update admin_books.php

--- admin_books.php
+++ admin_books.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Manage Books</title>
+</head>
+<body>
 
+<h2>Books</h2>
+
+<a href="add_book.php">Add Book</a>
+
+<table border="1">
+    <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Age Rating</th>
+        <th>Status</th>
+        <th>Publisher</th>
+        <th>Actions</th>
+    </tr>
+</table>
+
+</body>
+</html>
