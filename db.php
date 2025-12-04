cc1b562004bf5f93ee91b8517f0e3080ed30f33f
CterGcia <chester.garcia.cics@ust.edu.ph>
2025-12-04 21:49:01 +0800

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,10 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "lib";
+$dbname     = "restaurant";
 
-$conn = new mysqli($servername, $username, $password, $dbname)
-?>
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
