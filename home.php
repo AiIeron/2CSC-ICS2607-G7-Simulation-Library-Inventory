30d2044894e8469e52808ccb10dd031071e6dbfd
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-04 20:25:11 +0800

    Update home.php

--- home.php
+++ home.php
@@ -11,6 +11,7 @@
 <ul>
     <li><a href="view_books.php">View Available Books</a></li>
     <li><a href="my_rentals.php">My Rentals</a></li>
+    <li><a href="rent_book.php">Rent a Book</a></li>
     <li><a href="profile.php">My Profile</a></li>
     <li><a href="logout.php">Logout</a></li>
 </ul>
