230781e4c4160a9509ac0487f12ba20fa294d072
CterGcia <chester.garcia.cics@ust.edu.ph>
2025-12-04 21:15:26 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,20 +1,9 @@
 <?php
+// Database credentials
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username   = "root";
+$password   = "";
+$dbname     = "lib";
 
-$conn = new mysqli($servername, $username, $password);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "CREATE DATABASE lib";
-if ($conn->query($sql) === TRUE) {
-    echo "Proceed, Databaaes has been created";
-} else {
-    echo "Error creating lib" . $conn->error;
-}
-
-$conn->close();
+$conn = new mysqli($servername, $username, $password, $dbname)
 ?>
