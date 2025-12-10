6e33d76864572eff5b019eda5a57dbb138e042b5
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-10 14:38:20 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -9,8 +9,18 @@ session_destroy();
 <head>
     <title>Logging Out...</title>
     <meta http-equiv="refresh" content="1; URL=login.php">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+<div class="container">
+
+<div class="auth-box">
     <p>You have been logged out. Redirecting...</p>
+</div>
+
+</div>
+
 </body>
 </html>
+
