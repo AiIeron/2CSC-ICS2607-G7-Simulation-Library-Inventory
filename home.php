4deb974724297b2bb41102ea29c09a54b5f25e19
gabrielbenedictpanalcics <gabrielbenedict.panal.cics@ust.edu.ph>
2025-12-10 14:37:03 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,18 +1,26 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html>
 <head>
     <title>Library Home</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+<div class="container">
+
 <h2>Welcome!</h2>
 
+<div class="auth-box">
 <ul>
-    <li><a href="view_books.php">View Available Books</a></li>
-    <li><a href="my_rentals.php">My Rentals</a></li>
-    <li><a href="profile.php">My Profile</a></li>
-    <li><a href="register.php">Logout</a></li>
+    <li><a href="view_books.php" class="btn">View Available Books</a></li>
+    <li><a href="my_rentals.php" class="btn">My Rentals</a></li>
+    <li><a href="profile.php" class="btn">My Profile</a></li>
+    <li><a href="register.php" class="btn btn-danger">Logout</a></li>
 </ul>
+</div>
+
+</div>
 
 </body>
 </html>
+
